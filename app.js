05c7f5a5c02bf4efe6968cae29b36d56f52902c1
Mayank Chauhan <124248806+Mayankk499@users.noreply.github.com>
2025-08-23 12:49:21 +0530

    Invoking LLMs

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("Welcome  Gen AI");
+import Groq from "groq-sdk";
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+
+async function main() {
+  const completion = await groq.chat.completions.create({
+    model: "llama-3.3-70b-versatile",
+    messages: [
+      {
+        role: "user",
+        content: "Hi",
+      },
+    ],
+  });
+
+  console.log(completion);
+}
+
+main();
