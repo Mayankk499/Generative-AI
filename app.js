8f1e043e4e9b3c66f446c9f8bf28425a1f01ed2d
Mayank Chauhan <124248806+Mayankk499@users.noreply.github.com>
2025-08-24 14:33:25 +0530

    Add Punycode library with full implementation and package configuration
    - Introduced `punycode` package version 2.3.1 with complete source files.
    - Added `punycode.js` and `punycode.es6.js` for compatibility with various JavaScript environments.
    - Implemented core functions for encoding and decoding Punycode strings.
    - Included utility functions for handling Unicode code points and domain name mapping.
    - Configured package.json with necessary metadata, scripts, and dependencies.

--- app.js
+++ app.js
@@ -1,69 +1,105 @@
+import readline from 'node:readline/promises'
 import Groq from "groq-sdk";
+import { tavily } from "@tavily/core";
+
+const tvly = tavily({ apiKey: process.env.AVILY_API_KEY });
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 async function main() {
-  const completion = await groq.chat.completions.create({
-    // top_p: 0.2,
-    model: "llama-3.3-70b-versatile",
-    temperature: 0,
-    messages: [
-      {
-        role: "system",
-        content: "A smart personal assistant.",
-      },
-      {
-        role: "user",
-        content: "When was iphone 16 launched?",
-      },
-    ],
-    tools: [
-      {
-        type: "function",
-        function: {
-          name: "webSearch",
-          description: "Search the latest information and real time data on the internet.",
-          parameters: {
-            type: "object",
-            properties: {
-              location: {
-                type: "string",
-                description: "The search query to perform search on.",
+  const rl = readline.createInterface({input: process.stdin, output: process.stdout})
+  const messages = [
+    {
+      role: "system",
+      content: `A smart personal assistant. 
+      current datetime: ${new Date().toUTCString()}`,
+    },
+    // {
+    //   role: "user",
+    //   content: "What is the current weather in New delhi?",
+    // },
+  ];
+
+  while (true) {
+    const question = await rl.question('You: ')
+
+    if(question === 'bye'){
+      break;
+    }
+    messages.push({
+      role: 'user',
+      content: question,
+    })
+
+    while (true) {
+      const completion = await groq.chat.completions.create({
+        // top_p: 0.2,
+        model: "llama-3.3-70b-versatile",
+        temperature: 0,
+        messages: messages,
+        tools: [
+          {
+            type: "function",
+            function: {
+              name: "webSearch",
+              description:
+                "Search the latest information and real time data on the internet.",
+              parameters: {
+                type: "object",
+                properties: {
+                  location: {
+                    type: "string",
+                    description: "The search query to perform search on.",
+                  },
+                },
+                required: ["query"],
               },
             },
-            required: ["query"],
           },
-        },
-      },
-    ],
-    tool_choice: "auto"
-  });
+        ],
+        tool_choice: "auto",
+      });
 
-  const toolsCalls = completion.choices[0].message.tool_calls
+      messages.push(completion.choices[0].message);
 
-  if(!toolsCalls){
-    console.log(`AI Assistant: ${completion.choices[0].message.content}`);
-    return;
-  };
+      const toolsCalls = completion.choices[0].message.tool_calls;
 
-  for(const tool of toolsCalls){
-    console.log('tool: ', tool);
+      if (!toolsCalls) {
+        console.log(`AI Assistant: ${completion.choices[0].message.content}`);
+        break;
+      }
 
-    const functionName = tool.function.name;
-    const functionParams = tool.function.arguments;
+      for (const tool of toolsCalls) {
+        // console.log("tool: ", tool);
 
-    if(functionName === 'webSearch'){
-      const toolResult = await webSearch(JSON.parse(functionParams));
-      console.log('Tool result: ', toolResult);
-      
+        const functionName = tool.function.name;
+        const functionParams = tool.function.arguments;
+
+        if (functionName === "webSearch") {
+          const toolResult = await webSearch(JSON.parse(functionParams));
+          // console.log("Tool result: ", toolResult);
+
+          messages.push({
+            tool_call_id: tool.id,
+            role: "tool",
+            name: functionName,
+            content: toolResult,
+          });
+        }
+      }
     }
-    
   }
-
-  // console.log(JSON.stringify(completion.choices[0].message, null, 2));
+  rl.close();
 }
 
 main();
 
 async function webSearch({ query }) {
-  return "iPhone was launched on 20 sept 2024.";
+  const response = await tvly.search(query);
+  // console.log("Response:", response);
+
+  const finalResult = response.results
+    .map((result) => result.content)
+    .join("\n");
+
+  return finalResult;
 }
